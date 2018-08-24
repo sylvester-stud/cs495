555e75708ff63102eb694ebee1bd893e21b60c27
Christian Sylvester <christiansylvester2@gmail.com>
2018-08-23 17:31:06 -0700

    lab3_3 WORKS FINALLYgit add --all!

--- lab3_3.asm
+++ lab3_3.asm
@@ -9,16 +9,16 @@ _start:
 	mov rax, 41
 	mov rdi, 2			;inet
 	mov rsi, 1			;sock_stream
-	mov rdx, 0			;proto
+	mov rdx, 0			;protocol
 	syscall	;sys_socket
 	mov rdi, rax			;fd
 	mov rax, 42
-	mov rsi, ;	need the pointer to the value not the data@ rsp [rsp]			;sock_addr
 	mov rdx, 16			;addrlen
 	mov dword [rsp-4], 0x0100007f	;inet addr 127.0.0.1
 	mov word [rsp-6], 0x5c11	;port 4444
 	mov byte [rsp-8], 2		;inet
 	sub rsp, 8			;stack alignment
+ 	mov rsi, rsp;	need the pointer to the value not the data@ rsp [rsp]			;sock_addr
 	syscall ;sys_connect
 	mov rax, 44
 	mov rsi, msg			;send hello world
